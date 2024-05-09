772f8d475062fe27de0d34202b18716272cf683d
Jorge Ramirez <informatica@fruveco.com>
2024-05-09 10:10:38 +0200

    Actuaizado web.php

--- routes/web.php
+++ routes/web.php
@@ -26,22 +26,35 @@ use Inertia\Inertia;
 //  });
 
 // Rutas de la app
-Route::prefix('/cce')
+Route::prefix('cce')
     ->group(function () {
         Route::get('/', function () {
             return Inertia::render('Bienvenida');
         });
 
-        Route::prefix('/auth')
+        Route::prefix('auth')
             ->group(function () {
                 Route::controller(UserController::class)
                     ->group(function () {
                         Route::get('/login', 'login_form')
                             ->name('login');
+                        Route::post('/login', 'login')
+                            ->name('validar_login');
                         Route::get('/registro', 'register_form')
                             ->name('registro');
+                        Route::post('/registro', 'store')
+                            ->name('nuevo_usuario');
+                        Route::get('/reset-clave', 'reset_form')
+                            ->name('reset_clave');
+                        Route::get('/validar-email', function () {
+                          return Inertia::render('_EmailValidation');  
+                        })
+                            ->name('verification.notice');
+                        Route::post('/reset-clave', 'reset_form')
+                            ->name('guardar_clave');
                     });
-            });
+            })
+            ->middleware('auth');
     });
 
 // require __DIR__ . '/auth.php';
