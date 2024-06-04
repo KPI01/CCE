d712e864edfad535a1bf0753c8cc3d342136deb5
Jorge Ramirez <informatica@fruveco.com>
2024-06-04 16:08:04 +0200

    Modificacion de configuracion

--- config/session.php
+++ config/session.php
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(strtolower(env('APP_NAME_ABBREVIATION', 'laravel')), '_').'_session'
     ),
 
     /*
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,3 +1,5 @@
+export type UUID = `${string}-${string}-${string}-${string}-${string}`
+
 export interface User {
     id: number;
     name: string;
@@ -5,8 +7,46 @@ export interface User {
     email_verified_at: string;
 }
 
+type Modelo = {
+    id: UUID
+    created_at: string
+    updated_at: string
+}
+
+export interface Persona extends Modelo {
+    nombres: string
+    apellidos: string
+    tipo_id_nac: string
+    id_nac: string
+    email: string
+    tel: string
+    perfil: string
+    observaciones: string
+    ropo: {
+        id: string
+        tipo: string
+        caducidad: string
+        nro: string
+        tipo_aplicador: string
+    }
+    empresas?: Empresa[]
+}
+
+export interface Empresa extends Modelo {
+    nombre: string
+    nif: string
+    email: string
+    tel: string
+    codigo: string
+    perfil: string
+    direccion: string
+    observaciones: string
+    personas?: Persona[]
+}
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
     };
 };
+
