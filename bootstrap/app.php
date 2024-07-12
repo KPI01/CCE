512de0e81ed50de047315d6638f9b9f441c1ed4a
FruvecoDev <informatica@fruveco.com>
2024-07-12 13:39:37 +0200

    AGG: paquete telescope MODIF: configuraciones

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . "/../routes/web.php",
         commands: __DIR__ . "/../routes/console.php",
-        health: "/up"
+        health: "/up",
+        then: function () {
+            Route::middleware("web")->group([base_path("routes/app.php")]);
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\DuskServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
