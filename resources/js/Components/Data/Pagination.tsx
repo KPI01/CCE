4f17b43d0c5bc110763e50bb8b81bcf84b5c085b
FruvecoDev <informatica@fruveco.com>
2024-07-25 09:12:23 +0200

    MODIF: muestra "1" cuando no existen páginas DEL: migraciones de telescope

--- resources/js/Components/Data/Pagination.tsx
+++ resources/js/Components/Data/Pagination.tsx
@@ -48,7 +48,7 @@ export function Pagination<TData>({ table }: DataTablePaginationProps<TData>) {
           className="flex w-fit items-center justify-center text-sm font-medium"
         >
           Página {table.getState().pagination.pageIndex + 1} de{" "}
-          {table.getPageCount()}
+          {table.getPageCount() || 1}
         </div>
         <div className="flex items-center space-x-2">
           <Button
