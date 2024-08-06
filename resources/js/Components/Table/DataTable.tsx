04a3c24838846ddbcf650bdf8726983a9b74d82c
FruvecoDev <informatica@fruveco.com>
2024-08-06 17:08:29 +0200

    MODIF: renombrada tabla donde esta el DataTable

--- resources/js/Components/Table/DataTable.tsx
+++ resources/js/Components/Table/DataTable.tsx
@@ -24,8 +24,8 @@ import {
 } from "@/Components/ui/table";
 import { Separator } from "@/Components/ui/separator";
 import { Pagination } from "./Pagination";
-import ColumnFilters from "@/Components/Data/ColumnFilters";
-import { ColumnToggle } from "@/Components/Data/ColumnToggle";
+import ColumnFilters from "@/Components/Table/ColumnFilters";
+import { ColumnToggle } from "@/Components/Table/ColumnToggle";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
