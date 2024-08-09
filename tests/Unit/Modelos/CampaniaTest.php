4c0f99ea97eb3b46da69699beed5ff8bbc985004
FruvecoDev <informatica@fruveco.com>
2024-08-09 14:38:12 +0200

    AGG: archivos necesarios

---
+++ tests/Unit/Modelos/CampaniaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Modelos;
+
+use PHPUnit\Framework\TestCase;
+
+class CampaniaTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
