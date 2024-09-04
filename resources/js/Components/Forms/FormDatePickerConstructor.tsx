a289b05826a08db3b860fe846eb0da5c8a73456c
FruvecoDev <informatica@fruveco.com>
2024-09-04 12:03:10 +0200

    Pequeños ajustes

--- resources/js/Components/Forms/FormDatePickerConstructor.tsx
+++ resources/js/Components/Forms/FormDatePickerConstructor.tsx
@@ -42,10 +42,10 @@ export default function FormDatePickerConstructor({
   triggerStyle,
   descripcion,
   disabled = false,
-  resetBtn = false,
   resetFn,
-  placeholder = "(Vacío)",
+  placeholder = "Selecciona una fecha",
 }: Props) {
+  console.debug("FormDatePickerConstructor(values)", value);
   const toId = id.includes(".") ? id.replace(".", "_") : id;
   const labelId = `label-${toId}`;
   const inputId = `input-${toId}`;
@@ -64,7 +64,7 @@ export default function FormDatePickerConstructor({
     </FormDescription>
   );
 
-  const gridCols = resetBtn ? "15ch 1fr auto" : "15ch 1fr";
+  const gridCols = resetFn ? "15ch 1fr auto" : "15ch 1fr";
   const customItemClass = cn("grid gap-x-2 items-center", itemClass);
   const customStyle = { gridTemplateColumns: gridCols, ...itemStyle };
   const customTriggerClass = cn("grid items-center", triggerClass);
@@ -92,9 +92,12 @@ export default function FormDatePickerConstructor({
                 "mt-0 pl-3 text-left font-normal",
                 !value && "text-muted-foreground",
               )}
-              value={value ? format(value, "dd/MM/yyyy") : placeholder}
             >
-              <span>{value ? format(value, "dd/MM/yyyy") : placeholder}</span>
+              <span>
+                {value === undefined
+                  ? placeholder
+                  : format(value, "dd/MM/yyyy")}
+              </span>
               <Input
                 id={inputId}
                 type="hidden"
@@ -108,20 +111,20 @@ export default function FormDatePickerConstructor({
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
           <Calendar
-            captionLayout="dropdown"
             id={calId}
             mode="single"
             locale={es}
-            selected={value === null ? undefined : value}
+            selected={value}
             onSelect={onChange}
             disabled={(date) => date < new Date()}
-            month={value || undefined}
+            defaultMonth={value}
             fromYear={new Date().getFullYear()}
           />
         </PopoverContent>
       </Popover>
-      {resetBtn && (
+      {resetFn && (
         <Button
+          id="reset-datePicker"
           type="button"
           size={"sm"}
           variant={"destructive"}
