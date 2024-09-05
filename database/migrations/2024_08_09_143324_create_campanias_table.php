8d66e7a52bd508350c9a2bee92bb816bb717d284
FruvecoDev <informatica@fruveco.com>
2024-09-05 17:11:11 +0200

    Migración terminada

--- database/migrations/2024_08_09_143324_create_campanias_table.php
+++ database/migrations/2024_08_09_143324_create_campanias_table.php
@@ -4,16 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('campanias', function (Blueprint $table) {
+        Schema::create("campanias", function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->char("nombre", 25);
+            $table->boolean("is_activa");
+            $table->date("inicio");
+            $table->date("fin");
+            $table->text("descripcion");
         });
     }
 
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('campanias');
+        Schema::dropIfExists("campanias");
     }
 };
