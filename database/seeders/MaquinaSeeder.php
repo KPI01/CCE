33b98aa7583b4b1bc0c6aa82bf47254aa118b0f9
FruvecoDev <informatica@fruveco.com>
2024-09-09 15:58:34 +0200

    Eliminadas variables innecesarias

--- database/seeders/MaquinaSeeder.php
+++ database/seeders/MaquinaSeeder.php
@@ -8,21 +8,18 @@ use Illuminate\Support\Facades\Schema;
 
 class MaquinaSeeder extends Seeder
 {
-    public int $c;
     public static int $count = 0;
 
     public function __construct(int $count = 25)
     {
-        $this->c = $count;
         $this::$count = $count;
     }
     public function run(): void
     {
         //
-        $c = $this->c;
         echo "Llenando la auxiliar: tipos_maquina ..." . PHP_EOL;
         $this->call(AuxTiposMaquinaSeeder::class);
-        echo "Creando maquinas ({$c}) ..." . PHP_EOL;
-        Maquina::factory($c)->create();
+        echo "Creando maquinas ({$this::$count}) ..." . PHP_EOL;
+        Maquina::factory($this::$count)->create();
     }
 }
