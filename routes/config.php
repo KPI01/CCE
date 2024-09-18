e6c84d3c156585d4edab80b925a23aadf0e7ad54
FruvecoDev <informatica@fruveco.com>
2024-09-18 17:25:46 +0200

    Ruta para ver el token

--- routes/config.php
+++ routes/config.php
@@ -1,11 +1,24 @@
 <?php
 use App\Http\Controllers\Auxiliares\MaquinaAuxiliaresController;
+use App\Http\Controllers\CultivoController;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("app/config")
     ->middleware(["auth", "verified"])
     ->group(function () {
+        Route::get(
+            uri: "/token",
+            action: function (): JsonResponse {
+                return response()->json(
+                    data: [
+                        "token" => csrf_token(),
+                    ]
+                );
+            }
+        );
         Route::prefix("auxiliares")->group(function () {
+            Route::resource("cultivo", CultivoController::class);
             Route::controller(MaquinaAuxiliaresController::class)->group(
                 function () {
                     $name = "aux_maquina";
