531a95160396754f71b139c6cb8f82036a6218c8
FruvecoDev <informatica@fruveco.com>
2024-10-08 11:34:26 +0200

    Reducción de columnas y corregida implementación del Table

--- resources/js/Pages/Recursos/Persona/Column.tsx
+++ resources/js/Pages/Recursos/Persona/Column.tsx
@@ -2,9 +2,9 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 
-import { Persona } from "@/types";
 import { DataTableColumnHeader } from "@/Components/DataTable/ColumnHeader";
 import Actions from "@/Pages/Recursos/Persona/Actions";
+import { Persona } from "..";
 
 export const columns: ColumnDef<Persona>[] = [
   {
@@ -90,68 +90,8 @@ export const columns: ColumnDef<Persona>[] = [
     },
   },
   {
-    accessorFn: (row) => {
-      if (row.ropo?.capacitacion) {
-        return row.ropo.capacitacion;
-      }
-      return null;
-    },
-    id: "ropo_capacitacion",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Capacitación ROPO" />
-    ),
-    enableColumnFilter: true,
-    enableHiding: true,
-    size: 250,
-    meta: {
-      header: "Capacitación ROPO",
-      key: "ropo_capacitacion",
-    },
-  },
-  {
-    accessorFn: (row) => {
-      if (row.ropo?.caducidad) {
-        return row.ropo.caducidad;
-      }
-      return null;
-    },
-    id: "ropo_caducidad",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Caducidad" />
-    ),
-    cell: (info) => {
-      if (info.getValue()) {
-        return new Date(info.getValue() as string).toLocaleDateString();
-      }
-    },
-    enableColumnFilter: true,
-    enableSorting: true,
-    enableHiding: true,
-    meta: {
-      header: "Caducidad Carnet ROPO",
-      key: "ropo_caducidad",
-    },
-  },
-  {
-    accessorFn: (row) => {
-      if (row.ropo?.nro) {
-        return row.ropo.nro;
-      }
-      return null;
-    },
-    id: "ropo_nro",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Nº Carnet" />
-    ),
-    enableColumnFilter: true,
-    enableSorting: false,
-    meta: {
-      header: "N° de Carnet ROPO",
-      key: "ropo_nro",
-    },
-  },
-  {
     id: "actions",
+
     cell: ({ row }) => {
       const persona = row.original;
 
--- resources/js/Pages/Recursos/Persona/Table.tsx
+++ resources/js/Pages/Recursos/Persona/Table.tsx
@@ -1,36 +1,17 @@
 "user server";
-
 import DataLayout from "@/Layouts/DataLayout";
-
 import { columns } from "./Column";
-
-import { Persona } from "@/types";
+import { Persona, TableState } from "..";
+import { DataTableProps } from "@/Components/DataTable";
 
 interface Props {
   data: Persona[];
   url: string;
+  state: TableState;
 }
 
 export default function Table({ data, url }: Props) {
-  const initVisibility = {
-    nombres: true,
-    apellidos: true,
-    id_nac: true,
-    email: true,
-    tel: true,
-    perfil: true,
-    ropo_capacitacion: false,
-    ropo_caducidad: false,
-    ropo_nro: false,
-  };
-
   return (
-    <DataLayout
-      title="Personas"
-      data={data}
-      columns={columns}
-      url={url}
-      initialVisibility={initVisibility}
-    />
+    <DataLayout title="Personas" data={data} columns={columns} url={url} />
   );
 }
